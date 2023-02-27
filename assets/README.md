d533754c5d7c913e5d497fcf33a8b1c6c992bb59
dongdong0925 <2499318755@qq.com>
2023-02-27 20:45:45 +0800

    first commit

---
+++ assets/README.md
@@ -0,0 +1,26 @@
+# 基于layui的响应式文章后台管理系统
+
+项目预览地址：
+      
+       . 账号：dongdong0925    
+      -  密码：222222   -  
+      -  或者自己注册账号~
+
+-   **简介**：大事件后台管理系统是一款基于layui框架设计的，用于文章（分类）的增删改查，富文本页发布文章，编辑裁剪封面的响应式文章后台管理系统。
+## 技术栈 
+### 前端
+框架/库：[layui框架](https://www.layui.site/)、[jQuery](https://jquery.com/)
+
+模板引擎：[art-template](http://aui.github.io/art-template/zh-cn/)
+
+数据可视化：[echarts](https://echarts.apache.org/examples/zh/index.html)
+
+富文本：**[tinymce](https://github.com/tinymce/tinymce)**
+
+图片裁剪工具：[cropperjs](https://github.com/fengyuanchen/cropperjs)
+### 后端
+
+接口文档：[https://www.showdoc.com.cn/escook/3707158761215217](https://www.showdoc.com.cn/escook/3707158761215217)
+## 界面展示
+![enter image description here](../README.assets/1677495230094.png)
+
--- assets/js/login.js
+++ assets/js/login.js
@@ -1,61 +1,61 @@
-$(function() {
-    // 点击“去注册账号”的链接
-    $('#link_reg').on('click', function() {
-      $('.login-box').hide()
-      $('.reg-box').show()
-    })
-     
-    // 点击“去登录”的链接
-    $('#link_login').on('click', function() {
-      $('.login-box').show()
-      $('.reg-box').hide()
-    })
+$(function () {
+  // 点击“去注册账号”的链接
+  $('#link_reg').on('click', function () {
+    $('.login-box').hide()
+    $('.reg-box').show()
+  })
+
+  // 点击“去登录”的链接
+  $('#link_login').on('click', function () {
+    $('.login-box').show()
+    $('.reg-box').hide()
+  })
 
 
-    //从layui里获取form对象
-    var form = layui.form
-    //通过 form.verify() 函数自定义校验规则：
-    form.verify({
-        // 自定义了一个叫做 pwd 校验规则
-        pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
-        // 校验两次密码是否一致的规则
-        repwd: function(value) {
-          // 通过形参拿到的是确认密码框中的内容
-          // 还需要拿到密码框中的内容
-          // 然后进行一次等于的判断
-          // 如果判断失败,则return一个提示消息即可
-          var pwd = $('.reg-box [name=password]').val()
-          if (pwd !== value) {
-            return '两次密码不一致！'
-          }
-        }
-      })
+  //从layui里获取form对象
+  var form = layui.form
+  //通过 form.verify() 函数自定义校验规则：
+  form.verify({
+    // 自定义了一个叫做 pwd 校验规则
+    pwd: [/^[\S]{6,12}$/, '密码必须6到12位，且不能出现空格'],
+    // 校验两次密码是否一致的规则
+    repwd: function (value) {
+      // 通过形参拿到的是确认密码框中的内容
+      // 还需要拿到密码框中的内容
+      // 然后进行一次等于的判断
+      // 如果判断失败,则return一个提示消息即可
+      var pwd = $('.reg-box [name=password]').val()
+      if (pwd !== value) {
+        return '两次密码不一致！'
+      }
+    }
+  })
 
-       // 监听注册表单的提交事件
-      $('#form_reg').on('submit', function(e) {
-        // 1. 阻止默认的提交行为
-        e.preventDefault()
-        // 2. 发起Ajax的POST请求
-        var data = {
-          username: $('#form_reg [name=username]').val(),
-          password: $('#form_reg [name=password]').val()
-        }
-        $.post('/api/reguser', data, function(res) {
-          if (res.status !== 0) {
-            return layer.msg(res.message)
-          }
-       layer.msg('注册成功，请登录！')
-          // 模拟人的点击行为
-          $('#link_login').click()
-        })
-      })
-      //导入 layer：
-      var layer = layui.layer
-      //调用 `layer.msg()` 提示消息：
+  // 监听注册表单的提交事件
+  $('#form_reg').on('submit', function (e) {
+    // 1. 阻止默认的提交行为
+    e.preventDefault()
+    // 2. 发起Ajax的POST请求
+    var data = {
+      username: $('#form_reg [name=username]').val(),
+      password: $('#form_reg [name=password]').val()
+    }
+    $.post('/api/reguser', data, function (res) {
+      if (res.status !== 0) {
+        return layer.msg(res.message)
+      }
       layer.msg('注册成功，请登录！')
+      // 模拟人的点击行为
+      $('#link_login').click()
+    })
+  })
+  //导入 layer：
+  var layer = layui.layer
+  //调用 `layer.msg()` 提示消息：
+  layer.msg('注册成功，请登录！')
 
-      // 监听登录表单的提交事件
-$('#form_login').submit(function(e) {
+  // 监听登录表单的提交事件
+  $('#form_login').submit(function (e) {
     // 阻止默认提交行为
     e.preventDefault()
     $.ajax({
@@ -63,7 +63,7 @@ $('#form_login').submit(function(e) {
       method: 'POST',
       // 快速获取表单中的数据
       data: $(this).serialize(),
-      success: function(res) {
+      success: function (res) {
         if (res.status !== 0) {
           return layer.msg('登录失败！')
         }
@@ -75,5 +75,5 @@ $('#form_login').submit(function(e) {
       }
     })
   })
-      
-  })
+
+})
